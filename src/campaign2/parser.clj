4d504829241ada0b6dcc732a28233246a326099b
Dekel Pilli <dekelpilli@gmail.com>
2022-01-21 00:47:18 +1100

    Add initial logic for parsing a5e spells from PDF

--- src/campaign2/parser.clj
+++ src/campaign2/parser.clj
@@ -1,7 +1,8 @@
 (ns campaign2.parser
   (:require [campaign2.state :as state]
             [campaign2.util :as u]
-            [jsonista.core :as json])
+            [jsonista.core :as json]
+            [clojure.string :as str])
   (:import (java.io File)))
 
 (def ^String dir "loot/data/5et/")
@@ -32,3 +33,68 @@
                   (into (sorted-map)))]
     (state/write-data! (delay mons) "monster")))
 
+(defn idx-in-bounds? [idx coll] (<= idx (dec (count coll))))
+(defn spell-level? [s]
+  (or (str/starts-with? s "Cantrip (")
+      (str/starts-with? s "1st-level (")
+      (str/starts-with? s "2nd-level (")
+      (str/starts-with? s "3rd-level (")
+      (str/starts-with? s "4th-level (")
+      (str/starts-with? s "5th-level (")
+      (str/starts-with? s "6th-level (")
+      (str/starts-with? s "7th-level (")
+      (str/starts-with? s "8th-level (")
+      (str/starts-with? s "9th-level (")))
+
+(def level-str->level-int {"Cantrip"   0
+                           "1st-level" 1
+                           "2nd-level" 2
+                           "3rd-level" 3
+                           "4th-level" 4
+                           "5th-level" 5
+                           "6th-level" 6
+                           "7th-level" 7
+                           "8th-level" 8
+                           "9th-level" 9})
+
+(defn extract-spell-lines [^String file-name]
+  (let [lines (->> (File. file-name)
+                   (slurp)
+                   (str/split-lines)
+                   (remove #(re-matches #"^(0|[1-9][0-9]*)$" %))
+                   (vec))]
+    (loop [spells []
+           lines lines]
+      (let [idx (loop [spell-level-lines 0
+                       idx 0]
+                  (let [line (nth lines idx)]
+                    (if (spell-level? line)
+                      (if (zero? spell-level-lines)
+                        (recur (inc spell-level-lines) (inc idx))
+                        (dec idx))
+                      (let [new-idx (inc idx)]
+                        (if (idx-in-bounds? new-idx lines)
+                          (recur spell-level-lines new-idx)
+                          idx)))))
+            [spell lines] (split-at idx lines)]
+        (if (or (empty? lines) (empty? spell))
+          spells
+          (if (zero? idx)
+            (conj spells spell)
+            (recur (conj spells spell) lines)))))))
+
+(defn convert-spells []
+  (let [spell-lines (extract-spell-lines "data/a5e/spells/a/spells.txt")]
+    (map (fn [spell]
+           (let [name (first spell)
+                 [level & type-strs] (str/split (second spell) #" ") ;TODO handle multi-line level str
+                 [school types] (str/split (str/join type-strs) #";")]
+             ;TODO read classes/casting time/range/target/components/duration/save info
+             ;TODO for spell description, remove newlines not preceded by period
+             {:name   name
+              :level  (level-str->level-int level)
+              :school (str/join (rest school))
+              :types  (when types
+                        (map #(str/replace % #"\)" "")
+                             (str/split types #",")))}))
+         spell-lines)))
