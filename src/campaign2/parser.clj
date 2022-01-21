862ff56d761e8146c9ef0006d087733064896f90
Dekel Pilli <dekelpilli@gmail.com>
2022-01-21 18:47:21 +1100

    Spell parsing progress

--- src/campaign2/parser.clj
+++ src/campaign2/parser.clj
@@ -46,16 +46,34 @@
       (str/starts-with? s "8th-level (")
       (str/starts-with? s "9th-level (")))
 
-(def level-str->level-int {"Cantrip"   0
-                           "1st-level" 1
-                           "2nd-level" 2
-                           "3rd-level" 3
-                           "4th-level" 4
-                           "5th-level" 5
-                           "6th-level" 6
-                           "7th-level" 7
-                           "8th-level" 8
-                           "9th-level" 9})
+(def ->level {"Cantrip"   0
+              "1st-level" 1
+              "2nd-level" 2
+              "3rd-level" 3
+              "4th-level" 4
+              "5th-level" 5
+              "6th-level" 6
+              "7th-level" 7
+              "8th-level" 8
+              "9th-level" 9})
+
+(def ->school {"abjuration"    "A"
+               "conjuration"   "C"
+               "divination"    "D"
+               "enchantment"   "E"
+               "evocation"     "V"
+               "illusion"      "I"
+               "necromancy"    "N"
+               "psionic"       "P"
+               "transmutation" "T"})
+
+(defn sanitise
+  ([coll]
+   (sanitise coll nil))
+  ([coll extra-keys]
+   (let [cmap (into {\space ""
+                     \,     ""} (map #(vector % "")) extra-keys)]
+     (map #(str/escape % cmap) coll))))
 
 (defn extract-spell-lines [^String file-name]
   (let [lines (->> (File. file-name)
@@ -83,18 +101,59 @@
             (conj spells spell)
             (recur (conj spells spell) lines)))))))
 
+(defn merge-until-next-section [unparsed-lines next-prefix] ;TODO change next-prefix to next-pred to cater for optional sections
+  (let [first-line (first unparsed-lines)
+        unparsed-lines (rest unparsed-lines)
+        next-idx (reduce #(if (str/starts-with? %2 next-prefix)
+                            (reduced %1)
+                            (inc %1))
+                         0 unparsed-lines)]
+    {:lines   (nthrest unparsed-lines next-idx)
+     :content (reduce str first-line (take next-idx unparsed-lines))}))
+
+(defn extract-spell-sections [spell-lines]
+  (loop [spell {:name   (first spell-lines)
+                :page   497 ;spell page start, don't care about specifics
+                :source "A5E"}
+         section :level
+         unparsed-lines (rest spell-lines)]
+    (case section
+      :level (let [{:keys [content lines]} (merge-until-next-section unparsed-lines "Classes: ")
+                   [level level-suffix] (str/split content #" \(" 2)
+                   [school types] (str/split level-suffix #";" 2)
+                   types (-> (str/split types #",")
+                             (sanitise [\)]))]
+               (recur
+                 (-> spell
+                     (assoc :level (->level level))
+                     (assoc :tags types)
+                     (assoc :school (->school school)))
+                 :classes
+                 lines))
+      :classes (let [{:keys [content lines]} (merge-until-next-section unparsed-lines "Casting Time: ")
+                     classes (-> content
+                                 (subs (count "Classes: "))
+                                 (str/split #",")
+                                 (sanitise [\)]))]
+                 (recur (assoc spell :classes {:fromClassList (map (fn [class] {:name   (str/capitalize class)
+                                                                                :source "A5E"}) classes)})
+                        :casting-time
+                        lines))
+      :casting-time (let [{:keys [content lines]} (merge-until-next-section unparsed-lines "Range: ")
+                          [number unit other] (-> (subs content (count "Casting Time: "))
+                                                  (str/split #" " 3))]
+                      (recur
+                        (-> spell
+                            (assoc :time {:number (u/->num number)
+                                          ;TODO normalise unit (remove plurals)
+                                          :unit   unit})
+                            (cond->
+                              (and other
+                                   (str/includes? (str/lower-case other) "ritual")) (assoc-in [:meta :ritual] true)))
+                        :range
+                        lines))
+      :range (assoc spell :range \?))))
+
 (defn convert-spells []
   (let [spell-lines (extract-spell-lines "data/a5e/spells/a/spells.txt")]
-    (map (fn [spell]
-           (let [name (first spell)
-                 [level & type-strs] (str/split (second spell) #" ") ;TODO handle multi-line level str
-                 [school types] (str/split (str/join type-strs) #";")]
-             ;TODO read classes/casting time/range/target/components/duration/save info
-             ;TODO for spell description, remove newlines not preceded by period
-             {:name   name
-              :level  (level-str->level-int level)
-              :school (str/join (rest school))
-              :types  (when types
-                        (map #(str/replace % #"\)" "")
-                             (str/split types #",")))}))
-         spell-lines)))
+    (map extract-spell-sections spell-lines)))
